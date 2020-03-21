bac67b239d6bf8ecc9e94e03fd7fc14375279731
heyingwen <heyingwen2006@qq.com>
2020-03-21 19:14:31 +0800

    Update build.groovy

--- src/org/devops/build.groovy
+++ src/org/devops/build.groovy
@@ -4,5 +4,12 @@ def Build(buildType,buildShell){
    def buildTools=["mvn":"maven","ant":"ant","gradle":"gradle","npm":"npm"]
    println("当前选择的构建类型为${buildType}")
    buildHome=tool buildTools[buildType]
-   sh "${buildHome/bin/${buildType}}"  ${buildShell}
+   if("${buildType}"=="npm"){
+      sh """
+	     export NODE_HOME=${buildHome} 
+		 export PATH=\$NODE_HOME/bin:\$PATH  
+		 ${buildHome}/bin/${buildType} ${buildShell}"""        	 
+    }else{
+      sh "${buildHome/bin/${buildType}}"  ${buildShell}
+    }
 }
